9989c325c72761e2845ad8448f3de677f0ffe789
sablion <sabrinasuzana07@gmail.com>
2025-01-29 10:44:53 -0300

    Initial commit

---
+++ lista2.js
@@ -0,0 +1,61 @@
+console.log("ex1")
+let a = 5
+let b = 10
+
+let resultado1 = a > b
+let resultado2 = a <= b
+let resultado3 = a == b
+
+console.log(resultado1)
+console.log(resultado2)
+console.log(resultado3)
+
+// -----------------------------------
+console.log("ex2")
+let idade = 17
+
+let VotoOpcional = idade >= 16
+let VotObrigatorio = idade >= 18
+let VotoObrigatorio = idade <= 70
+
+console.log(VotoOpcional)
+console.log(VotObrigatorio)
+console.log(VotoObrigatorio)
+
+// -----------------------------------
+console.log("ex3")
+let idadePessoa = 35
+
+let maiorIdade = idadePessoa >= 18
+let idoso = idadePessoa >= 60
+
+console.log(maiorIdade)
+console.log(idoso)
+
+// -----------------------------------
+console.log("ex4")
+let NomeUsuario = "admin"
+
+let usuario = NomeUsuario == "admin"
+
+console.log(usuario)
+
+// -----------------------------------
+console.log("ex5")
+let num = 21
+
+let divisivel = num % 3
+let div = num % 5
+
+console.log(divisivel)
+console.log(div)
+
+// -----------------------------------
+console.log("ex6")
+let saldoConta = 1000
+let saque = 500
+let saldoSuficiente = saldoConta - saque
+let saldoMaiorCem = saldoSuficiente > 100
+
+console.log(saldoSuficiente)
+console.log(saldoMaiorCem)
